dbf5ecc9c4539f0ef0d1acb089eb5e7cae53a6f6
Eugene Shakhsuvarov <5747062+ishakhsuvarov@users.noreply.github.com>
2025-06-12 08:14:55 -0500

    Initialize npmconfig2

---
+++ scripts/initializers/cart.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-cycle */
+import { getHeaders } from '@dropins/tools/lib/aem/configs.js';
+import { initializers } from '@dropins/tools/initializer.js';
+import { initialize, setFetchGraphQlHeaders } from '@dropins/storefront-cart/api.js';
+import { initializeDropin } from './index.js';
+import { fetchPlaceholders } from '../commerce.js';
+
+await initializeDropin(async () => {
+  setFetchGraphQlHeaders((prev) => ({ ...prev, ...getHeaders('cart') }));
+
+  const labels = await fetchPlaceholders();
+  const langDefinitions = {
+    default: {
+      ...labels,
+    },
+  };
+
+  return initializers.mountImmediately(initialize, { langDefinitions });
+})();
